c134f72e72fd6f8faa0df0ea39b4c485c590de9f
FJAraujoUHU <franciscojavier.araujo885@alu.uhu.es>
2021-11-17 18:04:50 +0100

    Paso 3: clase implementada y cumple todos los tests.

--- src/src/Account.java
+++ src/src/Account.java
@@ -6,20 +6,28 @@ package src;
  */
 public class Account {
     
+    private double balance;
+    
     public Account(double balance) {
-        /**Empty**/
+        this.balance = balance;
     }
     
     public double getBalance() {
-        return 0;
+        return balance;
     }
     
     public boolean withdraw(double amount) {
-        return true;
+        if (amount < 0) return false;               //Si es negativo, rechazar la operación
+        if (balance - amount < -500) return false;  //Si se sobrepasa el límite, rechazar
+        else {
+            balance -= amount;
+            return true;
+        }
     }
     
-    public void deposit(double amount) {
-        /**Empty**/
+    public void deposit(double amount) throws IllegalArgumentException {
+        if (amount < 0) throw new IllegalArgumentException("Amount must be a positive number.");
+        else balance += amount;
     }
     
 }
